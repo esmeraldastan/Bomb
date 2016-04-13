e7b5f37fbd45eab7a390b3816b5164c4e7b1c4dd
Esmeralda C. Lopez <esmeraldarekz99@gmail.com>
2016-04-13 11:50:38 -0700

    work on

--- expo.py
+++ expo.py
@@ -4,9 +4,9 @@ sec = 0
 print "Place the bomb here?\n\n>yes\n>no"
 command = raw_input('>')
 
-if command == "yes":
+if command == "place":
     while sec != 3:
-        print ">>>>>>", sec
+        #print ">>>>>>", sec
         time.sleep(0.8)
         sec += 1
     print "time is up"
