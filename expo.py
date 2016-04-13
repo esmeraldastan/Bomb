7e6942bd30162a2c0370d634db66da9c6e6346d1
Esmeralda C. Lopez <esmeraldarekz99@gmail.com>
2016-04-13 11:27:58 -0700

    Timer

--- expo.py
+++ expo.py
@@ -1,3 +1,16 @@
+import time 
+
+sec = 0 
 print "Place the bomb here?\n\n>yes\n>no"
+command = raw_input('>')
 
 if command == "yes":
+    while sec != 3:
+        print ">>>>>>", sec
+        time.sleep(0.8)
+        sec += 1
+    print "time is up"
+    
+
+
+
